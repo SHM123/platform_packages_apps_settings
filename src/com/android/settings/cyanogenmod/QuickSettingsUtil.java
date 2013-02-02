f19c0d923f98519d5bab96c02b2139d05479e203
Danesh Mondegarian <daneshm90@gmail.com>
2013-02-01 23:59:25 -0500

    QuietHours Tile (2/2)
    Allows user to toggle state of quiet hours
    
    Change-Id: I9d3234b993d250a7d43683b51c1c3254aaec262f
    
    Conflicts:
    
    	AndroidManifest.xml
    	src/com/android/settings/Settings.java
    	src/com/android/settings/cyanogenmod/QuickSettingsUtil.java

--- src/com/android/settings/cyanogenmod/QuickSettingsUtil.java
+++ src/com/android/settings/cyanogenmod/QuickSettingsUtil.java
@@ -59,6 +59,7 @@ public class QuickSettingsUtil {
     public static final String TILE_LTE = "toggleLte";
     public static final String TILE_WIMAX = "toggleWimax";
     public static final String TILE_NFC = "toggleNfc";
+    public static final String TILE_QUIETHOURS = "toggleQuietHours";
 
     private static final String TILE_DELIMITER = "|";
     protected static ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -114,6 +115,9 @@ public class QuickSettingsUtil {
         TILES.put(TILE_NFC, new QuickSettingsUtil.TileInfo(
                 TILE_NFC, R.string.title_tile_nfc,
                 "com.android.systemui:drawable/ic_qs_nfc_off"));
+        TILES.put(TILE_QUIETHOURS, new QuickSettingsUtil.TileInfo(
+                TILE_QUIETHOURS, R.string.title_tile_quiet_hours,
+                "com.android.systemui:drawable/ic_qs_quiet_hours_off"));
         TILES.put(TILE_SETTINGS, new QuickSettingsUtil.TileInfo(
                 TILE_SETTINGS, R.string.title_tile_settings,
                 "com.android.systemui:drawable/ic_qs_settings"));
